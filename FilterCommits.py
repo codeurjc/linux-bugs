50986fc69f55cad2b7aaa2eea13220101634d121
Maes95 <michel.maes95@gmail.com>
2024-02-08 23:25:33 +0100

    Fix commit filter to obtaine the next 100 commits

--- FilterCommits.py
+++ FilterCommits.py
@@ -4,5 +4,5 @@ with open('linux-commits-2023-11-12_random-filtered.json') as fd:
     commits_json = json.load(fd)
 
 with open('linux-commits-2023-11-12_random-filtered-130-230.json', 'w') as f:
-    json.dump(commits_json[129:229], f)
+    json.dump(commits_json[130:230], f)
 
