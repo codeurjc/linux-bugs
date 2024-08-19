7e0c7fead0fda3e773cd3a88b372bd37bddb3aee
Maes95 <michel.maes95@gmail.com>
2024-08-19 19:39:50 +0200

    Re-write to fit Jesus specifications

--- lts-finder/README.md
+++ lts-finder/README.md
@@ -1,22 +1,17 @@
-# Upstream Commits Finder
+# Searching annotated commits in upstream commits from kernel stable branches
 
 ## Objetives
 
 - Locate all upstream commits listed in kernel.org from the following releases/branches: 6.6, 6.1, 5.15, 5.10, 5.4, and 4.19
 - From the 1000 commits annotated by us, check which ones are also upstreamed commits
 
-## What script does
+## Locate all upstream commits listed in kernel.org
 
 To track the Long-Term Support (LTS) versions listed on kernel.org (currently 6.6, 6.1, 5.15, 5.10, 5.4, and 4.19), we gather all commits for each of these branches. This can be done by checking the changelog files for each branch (e.g., 6.6.x, 6.1.x, 5.15.x, etc.). 
 
-For each commit mentioned in these changelogs, we look for the "upstream commit" line in the commit comment. 
-Then, we tried to find this commit in Linus Torvalds’ repository (which we are tracking) to see if we are annotating that commit.
+In summary, the script "upstreamCommitsFinder.py" accesses this online directory and retrieves all "upstream" commits for each branch.
 
-In summary, the script accesses this directory and retrieves all "upstream" commits for each branch.
-
-The final output is a file per branch, named after the branch (e.g., 5.0.1), with each line containing a commit hash.
-
-## Usage
+The final output is a file per branch, named after the branch (e.g., 5.0.1), with each line containing an upstream commit hash.
 
 Requirements:
 - Python +3.10
@@ -32,12 +27,16 @@ The script have two main functions:
 
 - ```get_branch(branch_id):``` This function takes a branch ID (6.6, 6.1, 5.15, 5.10, 5.4 or 4.19) and returns a list of commit hashes for each changelog that will be store in `results/` folder using the name of the changelog
 ```bash
-python UpstreamCommitsFinder.py ../linux-commits-2023-11-12_random-filtered-1.json --all
+python UpstreamCommitsFinder.py -b 6.1
 ```
-- ```get_all():``` This function retrieves the current list of branches and uses get_branch() on each of them. 
+- ```get_all():``` This function retrieves the current list of branches and uses get_branch() on each of them. In addition, it generates a CSV (_upstream_commits.csv_) with all upstream commits found from all branches. 
 ```bash
-python UpstreamCommitsFinder.py ../linux-commits-2023-11-12_random-filtered-1.json -b 6.1
+python UpstreamCommitsFinder.py --all
 ```
 
 
 Additionally, there is a ```--cache``` that could be used to cache downloaded files. If this flag is present, the script should check if the file is already in the cache before downloading it. This would involve adding a caching boolean to the functions and using a `cache/` directory to store the downloaded files.
+
+## Look for annotated commits in upstream commits
+
+...
---
+++ lts-finder/upstreamCommitsFinder.py
@@ -0,0 +1,76 @@
+import json
+from bs4 import BeautifulSoup 
+import requests 
+import re 
+import os
+import argparse
+import csv
+
+CURRENT_BRANCHES = ["6.6", "6.1", "5.15", "5.10", "5.4", "4.19"]
+
+BASE_URL = "https://cdn.kernel.org/pub/linux/kernel/"
+
+def get_branch(branch_id, cache=False):
+    upstream_commits = []
+    release_range = "v%d.x"%int(branch_id.split(".")[0])
+    # Change log page (e.g. https://cdn.kernel.org/pub/linux/kernel/v6.x)
+    changelog_list_page = getBsoupDocument(BASE_URL+release_range, cache)
+    for link in changelog_list_page.find_all('a', string=re.compile("^ChangeLog-"+branch_id)): 
+        # link is "ChangeLog-X.X"
+        print(BASE_URL+release_range+"/"+link.get('href'))
+        # changelog_page_html represent the page of changelog (e.g. https://cdn.kernel.org/pub/linux/kernel/v6.x/ChangeLog-6.6)
+        changelog_page_html = getHTMLDocument(BASE_URL+release_range+"/"+link.get('href'), cache)
+        # Find commit hash using regex
+        upstream_commits_in_changelog = re.findall("commit ([0-9a-f]{5,40}) upstream", changelog_page_html)
+        print(" > Upstream commits found: %d"%len(upstream_commits_in_changelog))
+        # Save upstream commits in a file
+        with open('results/%s.txt'%link.get('href'), 'w') as f:
+            for commit_hash in upstream_commits_in_changelog:
+                f.write("%s\n" % commit_hash)
+                upstream_commits.append((branch_id, commit_hash))
+    return upstream_commits
+                    
+def get_all(cache=False):
+    all_upstream_commits = []
+    for branch_id in CURRENT_BRANCHES:
+        all_upstream_commits += get_branch(branch_id, cache=cache)
+    
+    with open('upstream_commits.csv','w') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['branch','commit'])
+        for row in all_upstream_commits:
+            csv_out.writerow(row)
+
+def getBsoupDocument(url,cache=False): 
+    return BeautifulSoup(getHTMLDocument(url,cache) , 'html.parser')
+    
+def getHTMLDocument(url,cache=False):
+    fileName = url.split("/")[-1]
+    if cache and os.path.isfile('cache/%s.txt'%fileName):
+        response = requests.get(url)
+        with open('cache/%s.txt'%fileName, 'r') as f:
+            content = f.read()
+        return content
+    else:
+        response = requests.get(url)
+        with open('cache/%s.txt'%fileName, 'w') as f:
+            f.write(response.text)
+        return response.text
+        
+if __name__ == "__main__":
+    
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cache', action='store_true', help='Use cache to avoid downloading the same page multiple times')
+    parser.add_argument('--all', action='store_true', help='Get upstream commits in all branches')
+    parser.add_argument('-b', type=str, choices=CURRENT_BRANCHES, help='Get upstream commits in a specific branch')
+    args = parser.parse_args()
+    
+    if args.all:
+        print("Finding upstream commits in all branches")
+        get_all(cache=args.cache)
+    elif args.branch:
+        print("Finding upstream commits in branch: %s"%args.branch)
+        get_branch(args.branch, cache=args.cache)
+    else:
+        exit("Please specify a branch (e.g. -b 6.6) or use -all to get all branches")
